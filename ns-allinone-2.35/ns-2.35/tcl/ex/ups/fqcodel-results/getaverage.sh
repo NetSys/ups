8581f2319852022205632972f1f7bea105ac71a4
Radhika Mittal <radhika@c32.Millennium.Berkeley.EDU>
2016-02-16 16:01:54 -0800

    more corrections

--- ns-allinone-2.35/ns-2.35/tcl/ex/ups/fqcodel-results/getaverage.sh
+++ ns-allinone-2.35/ns-2.35/tcl/ex/ups/fqcodel-results/getaverage.sh
@@ -3,5 +3,5 @@
 for j in internet2-1Gbps-10Gbps-tcp-buf-34250-70
       do
         python findavgfct-fair.py fifo fq fqcodel5ms codelboxFQLstf5ms-116800 $j 1 6
-        python findavgdelay-fair.py fifo fq fqcodel5ms fqcodelLstf5ms codelboxFQLstf5ms-116800 $j 1 6
+        python findavgdelay-fair.py fifo fq fqcodel5ms codelboxFQLstf5ms-116800 $j 1 6
       done
