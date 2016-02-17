4d9c877e994fd2fa16af760a97ce34a362c9f0e7
Radhika Mittal <radhika@c32.Millennium.Berkeley.EDU>
2016-02-16 16:00:00 -0800

    more corrections

--- ns-allinone-2.35/ns-2.35/tcl/ex/ups/fqcodel-results/getaverage.sh
+++ ns-allinone-2.35/ns-2.35/tcl/ex/ups/fqcodel-results/getaverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for j in internet2-1Gbps-edge-codel-buf-34250-70
+for j in internet2-1Gbps-10Gbps-tcp-buf-34250-70
       do
         python findavgfct-fair.py fifo fq fqcodel5ms codelboxFQLstf5ms-116800 $j 1 6
         python findavgdelay-fair.py fifo fq fqcodel5ms fqcodelLstf5ms codelboxFQLstf5ms-116800 $j 1 6
